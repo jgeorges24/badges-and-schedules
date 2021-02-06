76c370c042242c2a97cbe23951261784f76eccf4
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-06 15:42:25 -0500

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
+# require'pry'
+
 # Write your code here.
+
+def badge_maker(name) #method for badge
+    return "Hello, my name is #{name}."
+
+
+end
+
+def batch_badge_creator(names)
+    names.map { |name| badge_maker(name) }
+end
+
+# binding.pry
+
+def assign_rooms(speakers)
+    new_aray = []
+    speakers.each_with_index do |name, i|
+        new_aray << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+    
+    end
+    return new_aray
+
+end
